b00a8014f37130fe9cd62b09a25c1e779ea7c0cf
SumithRaj <sumithraj05@SumithRaj.sr>
2023-05-28 22:08:55 +0530

    updated

--- src/Components/Contact/TextBox/TextBox.jsx
+++ src/Components/Contact/TextBox/TextBox.jsx
@@ -14,7 +14,7 @@ export default function TextBox() {
             .then((result) => {
                 alert("Email Sent")
             }, (error) => {
-                console.log(error.text);
+                alert("invalid email")
             });
     }
 
